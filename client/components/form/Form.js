895a18a1c40b554db9d1b785effd804974fe6829
Alan Terriaga <alanst32@gmail.com>
2018-09-10 17:39:12 +1000

    form fix

--- client/components/form/Form.js
+++ client/components/form/Form.js
@@ -1,37 +1,59 @@
 import React from 'react';
-import {FieldGroup} from 'react-bootstrap'
-import text from 'react-bootstrap-table2-filter/lib/src/components/text';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
-
+const styles = theme => ({
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit
+    }
+})
 
 
 class Form extends React.Component {
-    constructor(props, context){
 
-        super(props, context);
+    constructor(){
+        super();
 
-        this.state = {
-            firstName: text,
-            lastName: text,
-            birthDate: text,
-            country: text,
+        this.state={
+            person: {
+                firstName: '',
+                lastName: '',
+                country: ''
+            }
         }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event): void {
+        event.persist();
+
+        console.log(event);
+
+        this.setState((state) => state.person[event.target.id] = event.target.value);
     }
 
     render(){
         const { classes } = this.props;
 
         return(
-            <div className="Form">
-                <form>
-                    <FieldGroup id="firstName" type="text" label="First name" placeholder="Enter text"/>
-                    <FieldGroup id="lastName" type="text" label="Last name" placeholder="Enter text"/>
-                    <FieldGroup id="country" type="text" label="Country" placeholder="Enter text"/>
-                </form>
-            </div>
+            <form className={classes.container} noValidate autoComplete="off">
+                <TextField id="firstName" label="First name" className={classes.textField} value={this.state.firstName}
+                           onChange={this.handleChange} margin="normal"/>
+                <TextField id="lastName" label="Last name" className={classes.textField} value={this.state.lastName}
+                           onChange={this.handleChange} margin="normal"/>
+                <TextField id="country" label="Country" className={classes.textField} value={this.state.country}
+                           onChange={this.handleChange} margin="normal"/>
+            </form>
         );
     }
 }
 
 
-export default Form;
+Form.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Form);

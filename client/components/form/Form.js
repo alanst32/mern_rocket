75dd06e1aad318f665b1eb356aec8fa0a52000f1
alanst32 <alanst32@gmail.com>
2018-09-23 16:12:36 +1000

    form validation

--- client/components/form/Form.js
+++ client/components/form/Form.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import FormLabel from '@material-ui/core/FormLabel';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel'
 import SaveIcon from '@material-ui/icons/Save';
+import TextField from '@material-ui/core/TextField';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 
 import 'react-datepicker/dist/react-datepicker.css';
+import { timingSafeEqual } from 'crypto';
 
 const styles = theme => ({
     divStyle: {
@@ -28,6 +29,13 @@ const styles = theme => ({
         marginLeft: theme.spacing.unit,
         marginRight: theme.spacing.unit
     },
+    errorMsg: {
+        color: 'red',
+        fontSize: '10px',
+        width: '400px',
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit
+    },
     button: {
         margin: theme.spacing.unit,
         fontSize: 14
@@ -41,12 +49,19 @@ class Form extends React.Component {
         super();
 
         this.state={
+            error: {
+                firstName: '',
+                lastName: '',
+                country: '',
+                dateBirth: ''
+            },
             person: {
                 firstName: '',
                 lastName: '',
                 country: '',
                 dateBirth: moment()
             }
+            
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -55,37 +70,54 @@ class Form extends React.Component {
 
     handleChange(event) {
         event.persist();
+
+        var errorMsg = "";
         
+        if(event.target.value === ""){
+            errorMsg = "Field is required!";
+        }
+        this.setState((state) => state.error[event.target.id] = errorMsg);
+
         this.setState((state) => state.person[event.target.id] = event.target.value);
     }
 
     handleDateChange(date) {
         this.setState({ dateBirth: date });
+        this.setState((state) => state.error.dateBirth = "");
     }
 
     render(){
         const { classes } = this.props;
 
+        const errorInputMsg = (id) => (
+            <span className={classes.errorMsg}>{this.state.error[id]}</span>
+        )
+
         return(
             <form className={classes.container} noValidate autoComplete="off">
                 <div className={classes.divStyle}>
-                    <Input required="true" id="firstName" placeholder="First Name" className={classes.textField} 
-                         onChange={this.handleChange} />
+                    <TextField id="firstName" placeholder="First name" className={classes.textField} 
+                        onBlur={this.handleChange} error={this.state.person.firstName.length === 0 ? true : false }
+                        helperText={errorInputMsg("firstName")}/>
+                
                 </div>
                 <div className={classes.divStyle}>
-                    <span>
-                        <Input id="lastName" placeholder="Last Name" className={classes.textField} 
-                            required={true} onChange={this.handleChange} />
-                    </span>
+                    <TextField id="lastName" placeholder="Last Name" className={classes.textField} 
+                        onBlur={this.handleChange} error={this.state.person.lastName.length === 0 ? true : false }
+                        helperText={errorInputMsg("lastName")}/>
                 </div>
                 <div className={classes.divStyle}>
-                    <Input id="country" placeholder="Country" className={classes.textField} 
-                            required={true} onChange={this.handleChange} />
+                    <TextField id="country" placeholder="Country" className={classes.textField} 
+                        onBlur={this.handleChange} error={this.state.person.country.length === 0 ? true : false }
+                        helperText={errorInputMsg("country")}/>
                 </div>
                 <div className={classes.divStyle}>
                     <p></p>
                     <DatePicker id="dateBirth" placeholderText="Date of Birth" required={true} 
-                        className={classes.datePicker} selected={this.state.dateBirth} onChange={this.handleDateChange}/>
+                        className={classes.datePicker} selected={this.state.dateBirth} 
+                        onChange={this.handleDateChange} onBlur={this.handleChange}
+                        error={this.state.person.dateBirth.length === 0 ? true : false} helperText={this.state.error.dateBirth}/>
+                    <InputLabel id="dateBirth">{errorInputMsg("dateBirth")}</InputLabel>
                 </div>
                 <div className={classes.divStyle}>
                     <p></p>
